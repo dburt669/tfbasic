29ccf2c0505bbd75cea6821adc9760d8e5a0b378
Drew Burt <burtd667@gmail.com>
2022-04-15 20:14:13 -0400

    fixed storage account resource group reference

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "azurerm_storage_account" "sa" {
   account_tier = var.azurerm_storage_account_tier
   account_replication_type = var.azurerm_storage_account_replication_type
   min_tls_version = var.azurerm_storage_account_min_tls_version
-  resource_group_name = var.azurerm_resource_group
-  
+  resource_group_name = "${var.azurerm_resource_group}tfbasic"
+
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Basic Terraform repository
+
+This repository is a basic example of a terraform deployment
+
+creates a resource group and storage account
