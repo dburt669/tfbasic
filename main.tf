65b40c63881da5fe6078d332c0373e1a9f0c2fc3
Drew Burt <drew.burt@cancer.org>
2022-04-20 07:15:49 -0400

    added depends_on to stop resource creation failure.

--- main.tf
+++ main.tf
@@ -28,5 +28,8 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = var.azurerm_storage_account_replication_type
   min_tls_version = var.azurerm_storage_account_min_tls_version
   resource_group_name = "${var.azurerm_resource_group}tfbasic"
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 
 }
